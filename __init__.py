822d1cc89b7a19c38fd5fece46ed65759f78a539
Martin Baun <martin_baun@zoho.com>
2016-12-20 20:27:00 +0100

    added days.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 from randstr import randstr
 from hdcache import hdcache
+from days import all_days, work_days
+
+__all__ = ['randstr', 'hdcache', 'all_days', 'work_days']
---
+++ days.py
@@ -0,0 +1,6 @@
+
+all_days = ['monday', 'tuesday', 'wednesday',
+            'thursday', 'friday', 'saturday', 'sunday']
+
+
+work_days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
