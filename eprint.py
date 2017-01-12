7f26fbd49f583d529d0db5b7520c0792f519db60
Martin Baun <martin_baun@zoho.com>
2017-01-12 21:19:35 +0100

    added credits eprint.py

--- eprint.py
+++ eprint.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import sys
 
+# Credits go to MarcH from http://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python#14981125
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
