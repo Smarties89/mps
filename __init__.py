fa5e6fb439f2114aaee512dc8a2c18b1a6cc2b57
Martin Baun <martin_baun@zoho.com>
2017-01-12 21:18:53 +0100

    added eprint

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from randstr import randstr
 from hdcache import hdcache
 from days import all_days, work_days
+from eprint import eprint
 
 __all__ = ['randstr', 'hdcache', 'all_days', 'work_days']
