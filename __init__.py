6df484e5ded608ff7ea86783ddd2d09da3d65015
Martin Baun <martin_baun@zoho.com>
2017-03-17 09:19:02 +0100

    added deco_logger.py, this creates a decorator logger

--- __init__.py
+++ __init__.py
@@ -2,5 +2,6 @@ from randstr import randstr
 from hdcache import hdcache
 from days import all_days, work_days
 from eprint import eprint
+from deco_logger import deco_logger
 
 __all__ = ['randstr', 'hdcache', 'all_days', 'work_days']
---
+++ deco_logger.py
@@ -0,0 +1,15 @@
+
+def deco_logger(log, length=None):
+    length = 10 if length is None else 0
+    def log_function(f):
+        def wrapped(*args, **kwargs):
+            log.info(u"{} args:{}, kwargs:{}".format(
+                f.__name__,
+                ','.join([str(arg)[0:length] for arg in args]),
+                ','.join([str(arg)[0:length] for arg in kwargs]),
+                ))
+            return f(*args, **kwargs)
+
+        return wrapped
+
+    return log_function
