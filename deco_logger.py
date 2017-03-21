092afc39eb8800b7857c6a98fe008c13c667276a
Martin Baun <martin_baun@zoho.com>
2017-03-21 10:21:57 +0100

    fixed argument with deco_logger

--- deco_logger.py
+++ deco_logger.py
@@ -1,6 +1,6 @@
 
 def deco_logger(log, length=None):
-    length = 10 if length is None else 0
+    length = 10 if length is None else length
     def log_function(f):
         def wrapped(*args, **kwargs):
             log.info(u"{} args:{}, kwargs:{}".format(
