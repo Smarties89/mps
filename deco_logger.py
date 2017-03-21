2889f2152d79815dc937e19a223e26735d6e9a8b
Martin Baun <martin_baun@zoho.com>
2017-03-21 12:33:08 +0100

    unicode for deco_logger and funcname_override with for deco_logger

--- deco_logger.py
+++ deco_logger.py
@@ -1,13 +1,18 @@
+from functools import wraps
+
 
 def deco_logger(log, length=None):
     length = 10 if length is None else length
     def log_function(f):
+        @wraps(f)
         def wrapped(*args, **kwargs):
             log.info(u"{} args:{}, kwargs:{}".format(
                 f.__name__,
-                u','.join([str(arg)[0:length] for arg in args]),
-                u','.join([str(arg)[0:length] for arg in kwargs]),
-                ))
+                u','.join([unicode(arg)[0:length] for arg in args]),
+                u','.join([unicode(arg)[0:length] for arg in kwargs]),
+                ), extra={
+                    'funcname_override': f.__name__,
+                })
             return f(*args, **kwargs)
 
         return wrapped
