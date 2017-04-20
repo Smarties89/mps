3ec0b713d6a8f88b40d727ef508a6e9f6bd500a0
Martin Baun <martin_baun@zoho.com>
2017-04-21 00:02:28 +0200

    added docstring for randstr.py

--- randstr.py
+++ randstr.py
@@ -1,8 +1,19 @@
 import string
 import random
 
-# Slighty modified version of
-# http://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python
+
 def randstr(n=8):
+    """
+    randstr creates a random string of numbers and upper/lowercase characters.
+
+
+    >>> randstr()
+    "0YH58H9E"
+    >>> randstr(5)
+    "0ds34"
+
+    This code is slighty modified version of
+    http://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python
+    """
     chars = string.ascii_uppercase + string.digits
     return ''.join(random.choice(chars) for _ in range(n))
