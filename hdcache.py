28a3645f00c3bee2d95b029f263d8c213ac7d5d5
Martin Baun <martin_baun@zoho.com>
2017-04-30 12:12:02 +0200

    documentation hdcache.py

--- hdcache.py
+++ hdcache.py
@@ -15,8 +15,24 @@ def _getcachekey(args, kwargs):
 
 def hdcache(directory):
     """
-        Decorator function that permantly cache function calls on the
-        harddrive.
+        Decorator function that permantly cache function calls in a file 
+
+        Example
+
+	>>> @hdcache("cachedresult.db")
+	>>> def docomputation(x, y):
+	>>>    print("Doing hard computation for {} + {}".format(x, y))
+	>>>    return str(x+y)
+
+	>>> docomputation(2, 3, cachekey="2 plus 3")
+	"Doing hard computation for 2 + 5"
+	"5"
+	>>> docomputation(2, 3, cachekey="2 plus 3")
+	"5"
+
+	The second docomputation does not call the actual function but retrieves
+        the result from the file "cachedresult.db"
+        Moreover, next time the program is called the result will also be cached
     """
     def decs(func):
         def checkexists(*args, **kwargs):
